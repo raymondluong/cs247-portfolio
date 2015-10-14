0084493a16be0afaba29089b68e1667fa55308e3
Raymond Luong <rayluong92@gmail.com>
2015-10-14 02:28:38 -0700

    final

--- js/tile.js
+++ js/tile.js
@@ -1,8 +1,13 @@
+/* Script for navigating to the project pages as well as fading the other tiles on hover */
+
 $(document).on('ready', function(){
+	
 	$('.tile-img').on('click', function(e){
 		var url = e.currentTarget.id;
-	    window.open(url, '_blank');
+		if (url === 'apo') url = 'blank';
+	    window.open(url + '.html', '_self');
 	});
+	$('.tile').fadeIn(1000);
 	$('.tile').on('mouseover', function() {
 		$('.tile').not(this).addClass('tile-fade');
 	});
